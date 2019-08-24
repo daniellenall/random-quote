7261580d3b1f45cfc278bd01c95468ff2e282acf
Danielle <daniellernall@gmail.com>
2019-08-25 00:20:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Quote from './components/Quote';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        Random Quote
+      </header>
+      <Quote />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Quote.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Quote.css';
+
+const quotes = [
+    {quote: 'Quote 1', author: 'Edgar Alan Poe'},
+    {quote: 'Quote 1', author: 'Edgar Alan Poe'},
+];
+
+class Quote extends Component {
+    render() {
+        return(
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
+
+export default Quote;
